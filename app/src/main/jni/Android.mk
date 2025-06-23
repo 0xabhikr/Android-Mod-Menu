aba2ff4f702b320dd0c03339c89f1409a9c4b1ad
nepmods <nepmods@proton.me>
2025-06-23 14:03:16 +0545

    Remove: SubstrateHook Remove: And64InlineHook Added: DobbyHook

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -1,15 +1,19 @@
 LOCAL_PATH := $(call my-dir)
-include $(CLEAR_VARS)
 
-# Kittymemory
-KITTYMEMORY_PATH = KittyMemory
+# ─── Prebuilt: Dobby ───────────────────────────────
+include $(CLEAR_VARS)
+LOCAL_MODULE    := dobby
+LOCAL_SRC_FILES := Dobby/libraries/$(TARGET_ARCH_ABI)/libdobby.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+# ─── Prebuilt: Keystone ─────────────────────────────
 include $(CLEAR_VARS)
 LOCAL_MODULE    := Keystone
-LOCAL_SRC_FILES := $(KITTYMEMORY_PATH)/Deps/Keystone/libs-android/$(TARGET_ARCH_ABI)/libkeystone.a
+LOCAL_SRC_FILES := KittyMemory/Deps/Keystone/libs-android/$(TARGET_ARCH_ABI)/libkeystone.a
 include $(PREBUILT_STATIC_LIBRARY)
 
 # Here is the name of your lib.
-# When you change the lib name, change also on System.loadLibrary("") under OnCreate method on StaticActivity.java
+# When you change the lib name, change also on System.loadLibrary("") under OnCreate method on Main.java
 # Both must have same name
 include $(CLEAR_VARS)
 LOCAL_MODULE    := MyLibName
@@ -31,19 +35,13 @@ LOCAL_SRC_FILES := Main.cpp \
     Menu/Menu.cpp \
     Menu/Setup.cpp \
     Includes/Utils.cpp \
-	Substrate/hde64.c \
-	Substrate/SubstrateDebug.cpp \
-	Substrate/SubstrateHook.cpp \
-	Substrate/SubstratePosixMemory.cpp \
-	Substrate/SymbolFinder.cpp \
 	KittyMemory/KittyArm64.cpp \
     KittyMemory/KittyScanner.cpp \
     KittyMemory/KittyMemory.cpp \
     KittyMemory/KittyUtils.cpp \
     KittyMemory/MemoryPatch.cpp \
-    KittyMemory/MemoryBackup.cpp \
-	And64InlineHook/And64InlineHook.cpp \
+    KittyMemory/MemoryBackup.cpp
 
-LOCAL_STATIC_LIBRARIES := Keystone
+LOCAL_STATIC_LIBRARIES := Keystone dobby
 
 include $(BUILD_SHARED_LIBRARY)
